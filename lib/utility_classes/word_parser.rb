bd67392d897e05f5300c35f5e333f01e56961a20
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-07-12 22:55:40 -0500

    more comprehensive testing, for each tense

--- lib/utility_classes/word_parser.rb
+++ lib/utility_classes/word_parser.rb
@@ -1,28 +1,46 @@
 class WordParser
 
-	def self.parse_root(arabic_word)
-		parsed_word = arabic_word
+	def self.parse_root(parsed_word, original_word=parsed_word)
+		case parsed_word.length
+		# when 10
+		# when 9
+		# when 8
+		# when 7
+		# when 6
+		when 5
+			parsed_word.delete_at(4) if %w[aa n m].include?(parsed_word.last)
+			WordParser.parse_root(parsed_word, original_word)
 
-		%w[an on in].each { |symbol| parsed_word.delete(symbol) }
+		when 4
+			parsed_word.delete_at(3) if %w[t n w].include?(parsed_word.last)
+			parsed_word.delete_at(0) if parsed_word.first == "y"
+			parsed_word.delete("aa") if parsed_word.count("aa") == 1
+			WordParser.parse_root(parsed_word, original_word)
 
-		# while parsed_word.length > 3
-		10.times do
+		when 3
+			return parsed_word
 
-			if parsed_word.length > 4
-				parsed_word.delete("aa")
-				if parsed_word[1] == "s" && parsed_word[2] == "t"
-					parsed_word = parsed_word[3..parsed_word.length-1]
-				end
-			end
-
-			if parsed_word.length == 4
-				parsed_word.delete_at(3) if %w[t n].include?(parsed_word.last)
-				parsed_word.delete_at(0) if parsed_word.first == "y"
-			end
+		else
+			return original_word
 
 		end
+		# %w[an on in].each { |symbol| arabic_word.delete(symbol) }
 
-		return parsed_word
+		# while arabic_word.length > 3
+
+		# 	if arabic_word.length > 4
+		# 		arabic_word.delete("aa")
+		# 		if arabic_word[1] == "s" && arabic_word[2] == "t"
+		# 			arabic_word = arabic_word[3..arabic_word.length-1]
+		# 		end
+		# 	end
+
+		# 	if arabic_word.length == 4
+		# 		arabic_word.delete_at(3) if %w[t n].include?(arabic_word.last)
+		# 		arabic_word.delete_at(0) if arabic_word.first == "y"
+		# 	end
+
+		# end
 
 		# Won't work if ALIF is in root
 
--- spec/algorithms/word_parser_spec.rb
+++ spec/algorithms/word_parser_spec.rb
@@ -8,44 +8,77 @@ describe WordParser do
 
 			context "regular root" do
 
+				context "ism faal" do
+					it "parses the root for singular" do
+						arabic_word = %w[r aa k b]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[r k b]
+					end
+
+					it "parses the root for plural" do
+						arabic_word = %w[r k aa b]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[r k b]
+					end
+				end
+
+				context "ism mafool" do
+				end
+
+				context "masdar" do
+				end
+
 				context "present tense" do
-					it "parses the root (y H m 3)" do
+					it "parses the root for he" do
 						arabic_word = %w[y H m 3]
 						root = WordParser.parse_root(arabic_word)
 						root.should eq %w[H m 3]
 					end
-
-					it "parses the form (y H m 3)" do
-						arabic_word = %w[y H m 3]
-						word_form = WordParser.parse_word_form(arabic_word)
-						word_form.should eq 1
-					end
 				end
 
 				context "past tense" do
-					it "parses the root (m l k t)" do
+					it "parses the root for he" do
+						arabic_word = %w[m l k]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[m l k]
+					end
+
+					it "parses the root for i / you(s-m) / you(s-f) / she" do
 						arabic_word = %w[m l k t]
 						root = WordParser.parse_root(arabic_word)
 						root.should eq %w[m l k]
 					end
 
-					it "parses the form (m l k t)" do
-						arabic_word = %w[m l k t]
-						word_form = WordParser.parse_word_form(arabic_word)
-						word_form.should eq 1
-					end
-
-					it "parses the root (sh r b n aa)" do
-						arabic_word = %w[sh r b n aa]
+					it "parses the root for we(p)" do
+						arabic_word = %w[m l k n aa]
 						root = WordParser.parse_root(arabic_word)
-						root.should eq %w[sh r b]
+						root.should eq %w[m l k]
 					end
 
-					it "parses the form (sh r b n aa)" do
-						arabic_word = %w[sh r b n aa]
-						word_form = WordParser.parse_word_form(arabic_word)
-						word_form.should eq 1
+					it "parses the root for you(p-m)" do
+						arabic_word = %w[m l k t m]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[m l k]
 					end
+
+					it "parses the root for you(p-f)" do
+						arabic_word = %w[m l k t n]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[m l k]
+					end
+
+					it "parses the root for they(m)" do
+						arabic_word = %w[m l k w aa]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[m l k]
+					end
+
+					it "parses the root for they(f)" do
+						arabic_word = %w[m l k n]
+						root = WordParser.parse_root(arabic_word)
+						root.should eq %w[m l k]
+					end
+
 				end
 
 			end
@@ -270,76 +303,76 @@ describe WordParser do
 
 		end
 
-		context "Form X" do
+		# context "Form X" do
 
-			context "regular root" do
-				it "parses the root (y s t k sh f)" do
-					arabic_word = %w[y s t k sh f]
-					root = WordParser.parse_root(arabic_word)
-					root.should eq %w[k sh f]
-				end
+		# 	context "regular root" do
+		# 		it "parses the root (y s t k sh f)" do
+		# 			arabic_word = %w[y s t k sh f]
+		# 			root = WordParser.parse_root(arabic_word)
+		# 			root.should eq %w[k sh f]
+		# 		end
 
-				it "parses the form (y s t k sh f)" do
-					arabic_word = %w[y s t k sh f]
-					word_form = WordParser.parse_word_form(arabic_word)
-					word_form.should eq 10
-				end
+		# 		it "parses the form (y s t k sh f)" do
+		# 			arabic_word = %w[y s t k sh f]
+		# 			word_form = WordParser.parse_word_form(arabic_word)
+		# 			word_form.should eq 10
+		# 		end
 
-				it "parses the root (a s t kh d m)" do
-					arabic_word = %w[a s t kh d m]
-					root = WordParser.parse_root(arabic_word)
-					root.should eq %w[kh d m]
-				end
+		# 		it "parses the root (a s t kh d m)" do
+		# 			arabic_word = %w[a s t kh d m]
+		# 			root = WordParser.parse_root(arabic_word)
+		# 			root.should eq %w[kh d m]
+		# 		end
 
-				it "parses the form (a s t kh d m)" do
-					arabic_word = %w[a s t kh d m]
-					word_form = WordParser.parse_word_form(arabic_word)
-					word_form.should eq 10
-				end
+		# 		it "parses the form (a s t kh d m)" do
+		# 			arabic_word = %w[a s t kh d m]
+		# 			word_form = WordParser.parse_word_form(arabic_word)
+		# 			word_form.should eq 10
+		# 		end
 
-				it "parses the root (i s t H s aa n aa)" do
-					arabic_word = %w[i s t H s aa n aa]
-					root = WordParser.parse_root(arabic_word)
-					root.should eq %w[H s n]
-				end
+		# 		it "parses the root (i s t H s aa n aa)" do
+		# 			arabic_word = %w[i s t H s aa n aa]
+		# 			root = WordParser.parse_root(arabic_word)
+		# 			root.should eq %w[H s n]
+		# 		end
 
-				it "parses the form (i s t H s aa n aa)" do
-					arabic_word = %w[i s t H s aa n aa]
-					word_form = WordParser.parse_word_form(arabic_word)
-					word_form.should eq 10
-				end
-			end
+		# 		it "parses the form (i s t H s aa n aa)" do
+		# 			arabic_word = %w[i s t H s aa n aa]
+		# 			word_form = WordParser.parse_word_form(arabic_word)
+		# 			word_form.should eq 10
+		# 		end
+		# 	end
 
-			context "doubled root" do
-				it "parses the root (i s t D m aa m)" do
-					arabic_word = %w[i s t D m aa m]
-					root = WordParser.parse_root(arabic_word)
-					root.should eq %w[D m m]
-				end
+		# 	context "doubled root" do
+		# 		it "parses the root (i s t D m aa m)" do
+		# 			arabic_word = %w[i s t D m aa m]
+		# 			root = WordParser.parse_root(arabic_word)
+		# 			root.should eq %w[D m m]
+		# 		end
 
-				it "parses the form (i s t D m aa m)" do
-					arabic_word = %w[i s t D m aa m]
-					word_form = WordParser.parse_word_form(arabic_word)
-					word_form.should eq 10
-				end
+		# 		it "parses the form (i s t D m aa m)" do
+		# 			arabic_word = %w[i s t D m aa m]
+		# 			word_form = WordParser.parse_word_form(arabic_word)
+		# 			word_form.should eq 10
+		# 		end
 
-				context "with tanween" do
-					it "parses the root (i s t 3 d aa d an)" do
-						arabic_word = %w[i s t 3 d aa d an]
-						root = WordParser.parse_root(arabic_word)
-						root.should eq %w[3 d d]
-					end
+		# 		context "with tanween" do
+		# 			it "parses the root (i s t 3 d aa d an)" do
+		# 				arabic_word = %w[i s t 3 d aa d an]
+		# 				root = WordParser.parse_root(arabic_word)
+		# 				root.should eq %w[3 d d]
+		# 			end
 
-					it "parses the form (i s t 3 d aa d an) with tanween (an)" do
-						arabic_word = %w[i s t 3 d aa d an]
-						word_form = WordParser.parse_word_form(arabic_word)
-						word_form.should eq 10
-					end
-				end
+		# 			it "parses the form (i s t 3 d aa d an) with tanween (an)" do
+		# 				arabic_word = %w[i s t 3 d aa d an]
+		# 				word_form = WordParser.parse_word_form(arabic_word)
+		# 				word_form.should eq 10
+		# 			end
+		# 		end
 
-			end
+		# 	end
 
-		end
+		# end
 
 	end
 
